6e823227e238aab8e85d9927997eefc887a6bff5
Prashant Singh <prashantdineshsingh48@gmail.com>
2024-06-12 17:28:57 +0530

    Update backend

--- backend/index.js
+++ backend/index.js
@@ -2,20 +2,11 @@ import express from "express";
 import cors from "cors";
 import { routes } from "./routes/index.js";
 import { convertToApiError, handleError } from "./middlewares/errorHandling.js";
-import path from "path";
-import { fileURLToPath } from "url";
-
-// Use import.meta.url to derive __dirname
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.port || 8000;
+const port = 8000;
 
 app.use(cors());
-
-// serve static assets in production
-app.use(express.static(path.resolve(__dirname, "build")));
 
 // body parser
 app.use(express.json());
